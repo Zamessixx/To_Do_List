65296c75c439b210b6f3979ad38f266e91ab9f2b
AgressiveTime <agressivetime9@gmail.com>
2024-06-13 11:58:55 +0300

    second

--- main.py
+++ main.py
@@ -8,22 +8,36 @@ dp = Dispatcher(bot)
 
 database.init_db()
 
+
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        types.BotCommand(command='/start', description='команда для активации'),
+        types.BotCommand(command='/add', description='команда для доблавление задачи'),
+        types.BotCommand(command='/list', description='команда для того чтобы показать список задач'),
+        types.BotCommand(command='/delete', description='команда для удаление задача')
+    ]
+    await bot.set_my_commands(commands)
+
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
     await message.reply('Привет, я бот который может добавлять ваши задачи или напоминание')
 
 
 
-@dp.message_handler(commands= 'add')
+@dp.message_handler(commands='add')
 async def add(message: types.Message):
     task = message.get_args()
     if task:
         user_id = message.from_user.id
-        username = message.from_user.username
-        database.add_task(user_id, username, task)
-        await message.reply(f"Задача{task} добавлена")
-    else:
-        await message.reply('Пожалуйста, укажите команду через команду /add')
+        user_name = message.from_user.username
+        existing_tasks = database.get_task()
+        if any(task in t for t in existing_tasks):
+            await message.reply(f"Задача {task} уже существует")
+        else:
+            database.add_task(user_id, user_name, task)
+            await message.reply(f"Ваша задача {task} добавлена")
 
 
 @dp.message_handler(commands='list')
@@ -45,8 +59,10 @@ async def delete(message: types.Message):
         await message.reply('Укажите корректный id задач')
 
 
+async def on_startup(dispatcher):
+    await set_commands(dispatcher.bot)
 
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates= True)
+    executor.start_polling(dp, skip_updates= True, on_startup=on_startup)
